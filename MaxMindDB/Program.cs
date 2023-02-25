75be803791e38b0721ad13a6be58e19a41f94f8d
Joe Lippa <joe@bebo.dev>
2023-02-25 10:55:33 +0000

    initial commit

---
+++ MaxMindDB/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    https://dev.maxmind.com/geoip/geolite2-free-geolocation-data?lang=en#accessing-geolite2-free-geolocation-data
+    https://www.maxmind.com/en/accounts/current/geoip/downloads
+    
+    replace YOUR_LICENSE_KEY with a MaxMind license key
+    
+    curl -v 'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=YOUR_LICENSE_KEY&suffix=tar.gz.sha256' --output ./GeoLite2-Country.tar.gz.sha256
+    curl -v 'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=YOUR_LICENSE_KEY&suffix=tar.gz' --output ./GeoLite2-Country.tar.gz
+    cat ./GeoLite2-Country.tar.gz.sha256
+    sha256sum ./GeoLite2-Country.tar.gz
+    tar -xzf ./GeoLite2-Country.tar.gz
+*/
+
+using MaxMind.GeoIP2;
+
+using var reader = new DatabaseReader("./GeoLite2-Country.mmdb");
+Console.WriteLine(reader.TryCountry("24.24.24.24", out var countryResponse)
+    ? countryResponse!.Country.Name //United States
+    : "country not found");
---
+++ MaxMindDB/README.md
@@ -0,0 +1,3 @@
+## MaxMindDB
+
+Demonstrates how to retrieve country data from a MaxMind GeoLite2-Country.mmdb database file using the MaxMind GeoIP2 nuget package
